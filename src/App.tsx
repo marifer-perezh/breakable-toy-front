05c00f82e5c4c1b5a39f9f6ae74b93f485b1c8eb
marifer-perezh <maria.perez@encora.com>
2025-06-13 20:03:39 -0600

    front init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Inventory Manager</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
